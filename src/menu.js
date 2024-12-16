f4f317b7cff1786942ab8ebc7256e46fb4ee4642
unknown <moradejebbour401@gmail.com>
2024-12-16 15:26:27 +0100

    filters adding

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,19 @@
+
+
 //DEBE imprimir en pantalla la información de filtros.
+function mostrar_filtros_general(datos) {
+    // obtener el documento del dom a los filtros
+    let filtros_contenedor = document.getElementById('filters');
+    // recorer los datos
+    let filtro_boton = "";
+    datos.map(filtro => {
+        filtro_boton += `<button class="filter">${filtro}</button>`
+    });
+    filtros_contenedor.innerHTML = filtro_boton;
+}
 
 //DEBE imprimir en pantalla los productos, con su Título, descripción y precio en € y botón de añadir.
+function mostrar_menu_general() {
+    
+};
+export {mostrar_filtros_general};
