609298082b49261f19d20b21c324a5fef7a83b3c
PFBregon <bregon@gmail.com>
2024-12-17 11:58:45 +0100

    productos

--- src/cart.js
+++ src/cart.js
@@ -1,2 +1,1 @@
 //DEBE contener las funcionalidades del carrito de compras.
-
--- src/events.js
+++ src/events.js
@@ -1,10 +1,17 @@
-import {filters} from "../assets/data/data.js";
-import {mostrar_filtros_general} from "../src/menu.js";
+import {filters, products} from "../assets/data/data.js";
+import {mostrar_filtros_general, mostrar_productos} from "../src/menu.js";
 // evento para filtros
+
+
 // evento para productos
 document.addEventListener("DOMContentLoaded", ()=> {
     console.log(mostrar_filtros_general(filters));
+    console.log(mostrar_productos(products));
+    
 })
+
+
 // evento de click para el carrito 
+
 // evento click para pagar
 // evento para ver el factura
